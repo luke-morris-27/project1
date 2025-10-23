e9fd7efa4da4bd31e6a7700184b0d0cb31d4ad9b
luke-morris-27 <lukemorristx@outlook.com>
2025-10-23 13:14:36 -0500

    Update prefix.hs

--- prefix.hs
+++ prefix.hs
@@ -1,16 +1,18 @@
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 import Data.Char (isSpace, isDigit)
+import Data.List (isPrefixOf)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let interactive = case args of
-    [] -> True
-    ["-b"] -> False
-    ["--batch"] -> False
-    _ -> True
-  runCalc interactive[]
+    args <- getArgs
+    let interactive =
+          case args of
+              [] -> True
+              ["-b"] -> False
+              ["--batch"] -> False
+              _ -> True
+    runCalc interactive []
 
 -- Function to take user input
 
@@ -38,10 +40,11 @@ evalExpr (tok:rest) history
   | tok == "-" = evalOp (-) rest history
   | tok == "*" = evalOp (*) rest history
   | tok == "/" = evalOp (/) rest history
-  | "$" 'prefixOf' tok = case parseHistory tok history of
-    Just val -> Right (val, rest)
-    Nothing -> Left "Invalid history reference"
-  | other = case read tok :: Maybe Double of
+  | "$" `isPrefixOf` tok =
+        case parseHistory tok history of
+            Just val -> Right (val, rest)
+            Nothing  -> Left "Invalid History Reference"
+  | otherwise = case read tok :: Maybe Double of
     Just num -> Right (num, rest)
     Nothing -> Left "Invalid Token"
 
@@ -52,5 +55,11 @@ parseHistory ('$':digits) history
     in if index >= 0 && index < length history
       then Just (history !! index)
       else Nothing
-parseHistory _ = Nothing
- 
+parseHistory _ _ = Nothing
+
+evalOp :: (Double -> Double -> Double) -> [String] -> [Double]
+  -> Either String (Double, [String])
+evalOp f tokens history = do
+  (a, rest1) <- evalExpr tokens history
+  (b, rest2) <- evalExpr rest1 history
+  Right (f a b, rest2)
